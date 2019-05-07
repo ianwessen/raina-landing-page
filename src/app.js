69208b8f2fd53694fe0b3e1c21ff62be6f742623
Ian Wessen <ian@honeybook.com>
2019-05-06 21:03:14 -0700

    new scroll design

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 import './style.scss';
+
+$('.film-strip-frame').click(e => {
+    console.log(e.currentTarget.dataset.photoId)
+})
+
+const computeFocusThreshold = () => $(window).height() / 3;
+
+const updateFocus = () => {
+    // $('.film-strip-frame').each((i,frame) => {
+    //     const focusPoint = computeFocusThreshold();
+
+    //     const distanceFromTop = $(frame).offset().top - $(window).scrollTop();
+
+    //     if (distanceFromTop < focusPoint) {
+    //         $(frame).addClass('is-in-focus');
+    //     } else {
+    //         $(frame).removeClass('is-in-focus');
+    //     };
+    // });
+};
+
+const updateFocusThrottled = _.throttle(updateFocus, 200);
+
+$(window).scroll(updateFocusThrottled);
